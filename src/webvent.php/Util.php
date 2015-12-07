c66d3142d6e0f61c3a77b7706b3911e0e3a71638
Khan Mostafa <kmostafa@tcicredit.com>
2015-12-07 02:13:41 -0500

    scandir, except . and ..

--- src/webvent.php/Util.php
+++ src/webvent.php/Util.php
@@ -2,15 +2,17 @@
 
 
 class Util {
-    static function listAllFiles($base, $path) {
-        $files = [];
-        foreach(scandir($base.'/'.$path) as $f){
-            if(is_dir($f) && $f!='.' && $f!='..'){
-                $files = array_merge($files,self::listAllFiles($base, $path.'/'.$f));
-            }else{
-                array_push($files, $path.'/'.$f);
-            }
-        }
-        return $files;
+  static function listAllFiles($base, $path) {
+    $files = [];
+    foreach (scandir($base . '/' . $path) as $f) {
+      if ($f == '.' || $f == '..') {
+        //ignore
+      } elseif (is_dir($f)) {
+        $files = array_merge($files, self::listAllFiles($base, $path . '/' . $f));
+      } else {
+        array_push($files, $path . '/' . $f);
+      }
     }
+    return $files;
+  }
 }
