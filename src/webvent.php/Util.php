2b68e81a998265ee20a12c07e70637685d18c029
Khan Mostafa <kmostafa@tcicredit.com>
2015-12-07 02:18:40 -0500

    scandir: change in path descriptioning

--- src/webvent.php/Util.php
+++ src/webvent.php/Util.php
@@ -4,13 +4,13 @@
 class Util {
   static function listAllFiles($base, $path) {
     $files = [];
-    foreach (scandir($base . '/' . $path) as $f) {
+    foreach (scandir($base . $path) as $f) {
       if ($f == '.' || $f == '..') {
         //ignore
       } elseif (is_dir($f)) {
-        $files = array_merge($files, self::listAllFiles($base, $path . '/' . $f));
+        $files = array_merge($files, self::listAllFiles($base, $path . $f. '/' ));
       } else {
-        array_push($files, $path . '/' . $f);
+        array_push($files, $path . $f);
       }
     }
     return $files;
