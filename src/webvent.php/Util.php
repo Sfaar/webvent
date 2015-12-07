655546135de111701cb49a5176e7f6ebf79882a0
Khan Mostafa <kmostafa@tcicredit.com>
2015-12-07 02:00:12 -0500

    scandir

--- src/webvent.php/Util.php
+++ src/webvent.php/Util.php
@@ -6,7 +6,7 @@ class Util {
         $files = [];
         foreach(scandir($base.'/'.$path) as $f){
             if(is_dir($f)){
-                array_push($f, self::listAllFiles($base, $path.'/'.$f));
+                $files = array_merge($f, self::listAllFiles($base, $path.'/'.$f));
             }else{
                 array_push($f, $path.'/'.$f);
             }
