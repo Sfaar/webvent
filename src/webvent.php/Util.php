71bfd672872533b82060078d114ac95036f09574
Khan Mostafa <kmostafa@tcicredit.com>
2015-12-07 02:07:12 -0500

    scandir

--- src/webvent.php/Util.php
+++ src/webvent.php/Util.php
@@ -3,12 +3,13 @@
 
 class Util {
     static function listAllFiles($base, $path) {
+        print $base.' -> '.$path;
         $files = [];
         foreach(scandir($base.'/'.$path) as $f){
             if(is_dir($f) && $f!='.' && $f!='..'){
                 print_r(self::listAllFiles($base, $path.'/'.$f));
             }else{
-                print $f;
+                print $f."\n";
                 array_push($f, $path.'/'.$f);
             }
         }
